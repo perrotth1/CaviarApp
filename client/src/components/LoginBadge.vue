729e8053f63e5ac10473d6fec31831d0c16a6655
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-10-16 14:02:24 -0400

    made login badge

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="buttons" v-if="!Session.user">
+    <a class="button is-primary" href="/signup">
+      <strong>Sign up</strong>
+    </a>
+    <a class="button is-light" href="/login"> 
+        Log in 
+    </a>
+  </div>
+  <div v-else>
+      Hello {{name}}
+  </div>
+</template>
+
+
+<script>
+import Session from '../services/session';
+
+export default {
+    data (){
+        return ({
+            Session
+        })
+    },
+    methods: {
+        login(){
+            this.$router.push('/login');
+        }
+    },
+    computed: {
+        name() {
+            return this.Session.user.firstName + ' ' + this.Session.user.lastName;
+        }
+    }
+}
+
+</script>
