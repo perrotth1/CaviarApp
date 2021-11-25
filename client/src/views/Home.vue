189a0b3be2e3989a60827fc364a0b59af5f649e4
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-24 19:15:37 -0500

    implemented feed

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
 <div class="section">
-  <UserPostCard />
-
-  <UserPostCard />
-
-  <UserPostCard />
-
-  <UserPostCard />
+  <div class="feedPost" v-for="p in posts" :key="p._id">
+    <user-post-card :post="p" />
+  </div>
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import Session from '../services/session.js';
 import UserPostCard from '../components/UserPostCard.vue';
+import { GetFeed } from '../services/posts.js';
+
 
 export default {
   name: 'Home',
   components: {
     UserPostCard
+  },
+  data: () => ({
+    posts: null
+  }),
+  async mounted() {
+    this.posts = await GetFeed(Session.user.userHandle);
   }
 }
 </script>
